94638bab9715149f72e429a8ac2e388db52e04bd
Po-Nan Li <lishantreasure@gmail.com>
2016-10-06 16:02:59 -0700

    support year as input

---
+++ get_data.rb
@@ -0,0 +1,56 @@
+# encoding=utf-8 
+require 'open-uri'
+require 'net/http'
+require "json"
+
+if ARGV[0].nil?
+	year = "all"
+else
+	year = ARGV[0]
+end
+
+
+
+top_url = "http://rdc28.cwb.gov.tw/TDB/ntdb/pageControl/ty_warning?list=all"
+top_path = "http://rdc28.cwb.gov.tw/TDB/ctrl_typhoon_list/redirect2detail?typhoonId="
+
+uri = URI('http://rdc28.cwb.gov.tw/TDB/ctrl_typhoon_list/get_typhoon_list_table')
+res = Net::HTTP.post_form(uri, 'year' => year, 'model' => 'all')
+list = res.body
+
+pat1 = /<a class="typhoon_e_name" value="(\d+)">(\w+)<\/a>/
+pat2 = /<td align=left class="td_title">(.+)<\/td>\s+<td[\s\w\d"=]*>(.+)<\/td>/
+
+typhoons = list.scan(pat1).to_a
+
+typhoon_json = {}
+
+for typhoon in typhoons
+	id = typhoon[0]
+	typhoon_name = typhoon[1]
+	page_url = top_path + id
+	puts typhoon_name + ": " + page_url
+	tmp = open(page_url).read
+	raw_json = JSON.parse(tmp)
+	raw_table = raw_json["tytable"].scan(pat2).to_a
+	data = {}
+	for row in raw_table
+		data[row[0]] = row[1]
+	end
+	puts data
+	typhoon_json[id] = data
+end
+
+
+unless Dir.exists?("data")
+	Dir.mkdir("data")
+end
+
+File.open("data/#{year}.json", "w+:utf-8") do |i|
+	    i.write(JSON.pretty_generate(typhoon_json))
+end
+
+
+
+
+
